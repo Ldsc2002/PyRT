c8b83f855bbb3913d69ff00b31e01393bd598265
Ldsc2002 <luissantoscuellar@gmail.com>
2022-09-27 22:45:38 -0600

    Default colors in driver

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from PyRT.driver import *
 
 if __name__ == "__main__":
-    white = material(color(255, 255, 255))
-    red = material(color(255, 0, 0))
-
     objects = [      
-        cube((0, 0, -8), 2, white),  
-        sphere((0, 3, -15), 3, white),
-        sphere((0, 0, -10), 2, red)
+        cube((0, 0, -8), 2, getColor("red")),  
+        sphere((0, 3, -15), 3, getColor("green")),
+        sphere((0, 0, -10), 2, getColor("blue")),
     ]
 
     addToScene(objects)
