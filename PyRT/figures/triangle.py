97c2a94c90ff17a0a25eba77ab83171b3376b556
Ldsc2002 <luissantoscuellar@gmail.com>
2022-10-13 22:26:06 -0600

    Working with triangles

---
+++ PyRT/figures/triangle.py
@@ -0,0 +1,29 @@
+from PyRT.figures.figure import *
+
+class triangle(figure):
+    def __init__(this, vertices, material):
+        newVertices = [V3(*v) for v in vertices]
+
+        this.vertices = newVertices
+        this.material = material
+
+    def intersect(this, origin, direction):
+        v0, v1, v2 = this.vertices
+        normal = cross(sub(v1, v0), sub(v2, v0))
+        determinant = dot(normal, direction)
+
+        if abs(determinant) < 0.0001:
+            return None
+
+        distance = dot(normal, v0)
+        t = (dot(normal, origin) + distance) / determinant
+        if t < 0:
+            return None
+
+        point = sumV3(origin, mul(direction, t))
+        u, v, w = barycentric(v0, v1, v2, point)
+
+        if w < 0 or v < 0 or u < 0:
+            return None
+        
+        return intersect(distance, point, norm(normal))
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ glass = material("white", [0, 0.5, 0.1, 0.8], 125, 1.5)
 
 objects = [    
     cube((-2, 0, -10), 1, glossRed),
+    triangle(((-1, -1, -10), (1, -1, -10), (0, 1, -10)), mirror),
 ]
 
 setLight((-20, 20, 20), 2, "white")
